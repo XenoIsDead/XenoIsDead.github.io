d208a681bda6a732625ce15aaf562989ce0663e3
XenoIsDead <xenoisdead@protonmail.com>
2023-09-18 21:35:47 -0400

    2nd commit

---
+++ index.vue
@@ -0,0 +1,23 @@
+
+<script setup>
+import {ref} from 'vue'
+import { reactive } from 'vue'
+
+const x = 0
+const y = 0
+function moverandom(){
+    x = Math.floor(Math.random() * 1000);
+    y = Math.floor(Math.random() * 1000);
+}
+var img = document.getElementById('xenoimage');
+img.style.position = "absolute";
+img.style.left = x+'px';
+img.style.top = y+'px';
+
+</script>
+
+<template>
+    <div @click="moverandom" id = "xenoimage">
+        <a href="Xeno.gif"></a>
+    </div>
+</template>
